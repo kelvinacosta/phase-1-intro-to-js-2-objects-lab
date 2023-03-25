c1292135a6371de61a11f2630d4611a9638e9eb3
Kelvin Acosta <acosta.kelvin02@gmail.com>
2023-03-24 22:50:34 -0400

    complete objects lab

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution in this file!
+const employee = {
+    name:'Kelvin Acosta',
+    streetAddress: '1010605 sw'
+}
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    
+    //const employees = { ...employee,[key]:value}
+    const employees = { ...employee };
+    employees[key] = value;
+    return employees;
+    
+    
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    //employee[key] = value;
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key){
+    const employees = {...employee};
+    delete employees[key];
+    return employees;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+
+    return employee;
+}
+
